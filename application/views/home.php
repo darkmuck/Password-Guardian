b52b60352608a11552c407d0b4d7d454258ab38e
dcelasun <dcelasun@gmail.com>
2011-10-28 10:08:45 +0300

    whitespace fixes

--- application/views/home.php
+++ application/views/home.php
@@ -2,7 +2,7 @@
 
 	<div id="content">
 		<?php
-					
+		
 		//Show any messages
 		if($this->session->flashdata('message'))
 		{
@@ -10,21 +10,21 @@
 		}
 		?>
 		
-			<div id="login">
-				<form action="" method="post">
-					<fieldset class="login">
-						<label for="username">Username</label>
-						<input type="text" name="username" id="username" />
-					</fieldset>
-					<fieldset class="login">
-						<label for="password">Password</label>
-						<input type="password" name="password" id="password" />
-					</fieldset>
-					<fieldset class="submit">
-						<input type="submit" name="submit" value="Login" />
-					</fieldset>
-				</form>
-			</div>
+		<div id="login">
+			<form action="" method="post">
+				<fieldset class="login">
+					<label for="username">Username</label>
+					<input type="text" name="username" id="username" />
+				</fieldset>
+				<fieldset class="login">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" />
+				</fieldset>
+				<fieldset class="submit">
+					<input type="submit" name="submit" value="Login" />
+				</fieldset>
+			</form>
+		</div>
 	</div>
 <?php $this->load->view('footer');?>
 
