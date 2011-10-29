151756ddb7ccc1eaf1ca9f7e484f7f3b21476a73
Can Celasun <dcelasun@gmail.com>
2011-10-29 12:33:38 +0300

    jQuery & jQuery UI support

--- application/views/header.php
+++ application/views/header.php
@@ -1,8 +1,14 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
 <html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url().'themes/'.$this->config->item('config_theme').'/css/' ?>screen.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo css_url(); ?>/screen.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo css_url(); ?>/jquery-ui/jquery-ui-1.8.16.custom.css" />
+<script type="text/javascript" src="<?php echo js_url(); ?>/jquery-1.6.4.min.js"></script>
+<script type="text/javascript" src="<?php echo js_url(); ?>/jquery-ui-1.8.16.custom.min.js"></script>
+<script type="text/javascript">
+var actionurl = '<?php echo action_url(); ?>';
+</script>
 <title><?php if(isset($page_title)) {echo $page_title.' / ';}?><?php echo $this->config->item('site_title');?></title>
 <?php
 if (isset($extraHeaders))
