7b7a2d2da0dfe504ba537808f9764b7a06d1de23
Can Celasun <dcelasun@gmail.com>
2011-10-29 12:34:00 +0300

    Basic random password generator

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,5 +3,29 @@
 </div>
 
 </div><!--wrapper -->
+
+<script type="text/javascript">
+$(document).ready(function() {
+	$('#gen_pass').click(function() {
+		generatePassword();
+		return false;
+	});
+});
+
+function generatePassword()
+{
+	$.ajax(
+	{
+		type: "POST",
+		url: actionurl+"password/generate",
+		data: "",
+		success: function(response)
+		{
+			$('<div></div>').html('Copy &amp; paste this: '+response).dialog({title: 'Generated Password', modal:true });
+			
+		}
+	});
+};
+</script>
 </body>
 </html>
--- application/views/navigation.php
+++ application/views/navigation.php
@@ -4,5 +4,9 @@
 	<a href="<?php echo site_url(); ?>manager/categories">Categories</a>
 	<a href="<?php echo site_url(); ?>manager/settings">Settings</a>
 	<a href="<?php echo site_url(); ?>logout">Logout</a>
-			
+	
+	<a href="javascript:void(0);" class="image right">
+		<img id="gen_pass" src="<?php echo image_url(); ?>/add.png" alt="Generate password" title="Click here to generate a password."/>
+	</a>
+
 </div>
